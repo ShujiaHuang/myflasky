d0db69d746bf643c622e71f3ff6e7a793f658be5
Shujia Huang <huangshujia9@gmail.com>
2015-12-17 16:33:01 +0800

    use template

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
+"""
+Name: Shujia Huang
+Date: 2015-12-17
+"""
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello world</h1>' 
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name = name)
