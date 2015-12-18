ae4008f10419371e36fe289090ef967a84657a50
Shujia Huang <huangshujia9@gmail.com>
2015-12-18 12:00:15 +0800

    update the index route: adding 'GET' and 'POST' methods

--- app/views.py
+++ app/views.py
@@ -6,11 +6,19 @@ from flask import render_template
 from datetime import datetime
 
 from app import app
+from .forms import NameForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', 
+                           form = form,
+                           name = name,
+                           current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
