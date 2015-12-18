63394c3fb6ff59652d79e8cedd6e2c1fdc7ebfc0
Shujia Huang <huangshujia9@gmail.com>
2015-12-18 23:38:23 +0800

    setting config for sqlite database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 from app import views
--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # avoid 'Must provide secret_key to use csrf.'
 CSRF_ENDBLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+# SQLite database
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
