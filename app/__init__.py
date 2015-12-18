cde882690dd83b1146c5b2efeaf0d3bdf8ddef15
Shujia Huang <huangshujia9@gmail.com>
2015-12-18 10:55:55 +0800

    stay away from 'CSRF'

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
---
+++ config.py
@@ -0,0 +1,3 @@
+# avoid 'Must provide secret_key to use csrf.'
+CSRF_ENDBLED = True
+SECRET_KEY = 'you-will-never-guess'
