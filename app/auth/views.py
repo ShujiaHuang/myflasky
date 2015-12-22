820f17992cb3767bf0fd264ca04dea8a7cf008c0
Shujia Huang <huangshujia9@gmail.com>
2015-12-22 16:32:32 +0800

    user register

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,11 +3,13 @@ views function for authentication
 """
 from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import current_user
 
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
-
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,3 +28,17 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Now you can login.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
