b060542aaf20d359a6410586105e36802de9cf60
Shujia Huang <huangshujia9@gmail.com>
2016-01-04 11:52:12 +0800

    user gravatar

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from flask.ext.login import UserMixin, AnonymousUserMixin
 
 from . import db, login_manager
@@ -108,6 +108,16 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash_md5 = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+                url=url, hash=hash_md5, size=size, default=default, 
+                rating=rating)
     
     def __repr__(self):
         return '<User %r>' % self.username
